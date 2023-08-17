748153c968967b5f1b0e718a74eb50d309eadc08
ballanse <devblackholes@gmail.com>
2023-08-17 15:06:55 +0530

    added shoes size option,bug fix

--- src/components/alertDialog/SizeChart.tsx
+++ src/components/alertDialog/SizeChart.tsx
@@ -1,15 +1,80 @@
-import {View,TouchableOpacity} from 'react-native';
-import React from 'react';
-import { Box,Text } from 'native-base';
+import {View, TouchableOpacity} from 'react-native';
+import React, {useState, useRef} from 'react';
+import {
+  AlertDialog,
+  Box,
+  Button,
+  FlatList,
+  HStack,
+  Heading,
+  Spacer,
+  Text,
+  VStack,
+} from 'native-base';
+import {shoesSize} from '../../data/shoesSize';
 
 const SizeChart = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const onClose = () => setIsOpen(false);
+  const cancelRef = useRef(null);
+
   return (
     <Box>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => setIsOpen(!isOpen)}>
         <Text fontSize={'18'} underline>
           Size chart
         </Text>
       </TouchableOpacity>
+      <AlertDialog
+        leastDestructiveRef={cancelRef}
+        isOpen={isOpen}
+        onClose={onClose}>
+        <AlertDialog.Content>
+          <AlertDialog.CloseButton />
+          <AlertDialog.Header>Size chart</AlertDialog.Header>
+          <AlertDialog.Body>
+            <Box>
+              <HStack justifyContent={'space-between'}>
+                <Text underline>Size(UK)</Text>
+                <Text underline>To Fit Foot Length(cm)</Text>
+              </HStack>
+              <FlatList
+                data={shoesSize}
+                keyExtractor={item => item.id}
+                renderItem={({item}) => (
+                  <Box
+                    borderBottomWidth="1"
+                    _dark={{
+                      borderColor: 'muted.50',
+                    }}
+                    borderColor="muted.800"
+                    py="2">
+                    <HStack flexDirection={'row'} space={'60%'} textAlign={'center'}>
+                      <Text color="coolGray.800" bold >
+                        {item.size}
+                      </Text>
+
+                      <Text color="coolGray.600">{item.cm}</Text>
+                      <Spacer />
+                    </HStack>
+                  </Box>
+                )}
+              />
+            </Box>
+          </AlertDialog.Body>
+          <AlertDialog.Footer>
+            <Button.Group space={2}>
+              <Button
+                variant="unstyled"
+                colorScheme="coolGray"
+                onPress={onClose}
+                ref={cancelRef}>
+                Cancel
+              </Button>
+            </Button.Group>
+          </AlertDialog.Footer>
+        </AlertDialog.Content>
+      </AlertDialog>
     </Box>
   );
 };
---
+++ src/data/shoesSize.ts
@@ -0,0 +1,38 @@
+interface shoesSizeType {
+  id: string;
+  size: number;
+  cm: string;
+}
+
+export const shoesSize: shoesSizeType[] = [
+  {
+    id: '1',
+    size: 7,
+    cm: "28.0",
+  },
+  {
+    id: '2',
+    size: 8,
+    cm: "28.6",
+  },
+  {
+    id: '3',
+    size: 9,
+    cm: "30.0",
+  },
+  {
+    id: '4',
+    size: 10,
+    cm: "30.7",
+  },
+  {
+    id: '5',
+    size: 11,
+    cm: "31.0",
+  },
+  {
+    id: '6',
+    size: 12,
+    cm: "31.7",
+  },
+];
